4c2799c157944139ffe3a23b1a82aff56f4b1c98
figo <23335628@qq.com>
2015-07-26 16:02:05 +0800

    fix default ftlRoot

--- grunt/server.js
+++ grunt/server.js
@@ -3,8 +3,10 @@
 //===========freemarker FED system========================
 
 var fs = require('fs');
+var path = require('path');
 var config = JSON.parse(fs.readFileSync('./mock/config.json'));
-var ftlRoot = config.ftlRoot;
+var ftlRoot = config.ftlRoot || path.join(__dirname,'../demo');
+console.log(ftlRoot);
 var commonMock = config.globalData;
 var querystring = require('querystring');
 var Freemarker = require('freemarker.js');
